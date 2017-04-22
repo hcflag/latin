47a3b4e5acd23e698a8a53fcafbb38e006bf0338
Neel Smith <dnsmith.neel@gmail.com>
2017-04-22 16:13:42 -0400

    Add script for formatting CEX file

---
+++ scripts/cex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Assembles catalog and contents of all cataloged editions in a single file
+# in  CITE Exchange (cex) format, replacing all tab characters with the pound
+# sign "#". Writes to standard output: redirect as you like.
+#
+# Run this script from the root directory of the flag latin repository.
+#
+# Example:
+#
+#    ./scripts/cex.sh > flag.cex
+#
+
+cat cextemplates/catalogdiv.txt editions/flag_catalog.tsv cextemplates/catalogdata.txt  editions/cataloged/*tsv | sed -e $'s/\t/#/g'
